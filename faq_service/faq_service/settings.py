c4347f22388ccf8c2ced16541dee1d7816f5d288
alffy007 <jamesbond006aj@gmail.com>
2025-02-01 20:26:12 +0530

    docs: Update README with API examples

--- faq_service/faq_service/settings.py
+++ faq_service/faq_service/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'faq_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bharath_fd_db',
+        'USER': 'admin',
+        'PASSWORD': 'bharath_fd',
+        'HOST': 'db',  # Name of the database container service
+        'PORT': '5432',
     }
 }
+
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
